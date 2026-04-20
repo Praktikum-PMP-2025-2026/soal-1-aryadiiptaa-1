5d05b101c69dd7fcd61ba1afce1f756e8cf53a50
Ihsan Arya <ihsanaryafiisp@gmail.com>
2026-04-20 08:28:47 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,42 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : Data Manipulation and External Files
+ *   Hari dan Tanggal    : Senin, 24 April 2026
+ *   Nama (NIM)          : Ihsan Arya Pradipta (13224037)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : -1 dan kawand kawand
+ * 
+ */
 
-int main(void) {
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int N;
+    int jumlah = 0;
+    scanf("%d", &N);
+    int arr[N];
+    for(int i=0; i<N; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i=N-1; i>=0; i--){
+        if(arr[i]==-1){
+            if(arr[i-1]!=-1 && arr[i+1]!=-1){
+                arr[i] = floor((arr[i+1]+arr[i-1])/2);
+            }
+            else if(arr[i-1]!=-1){
+                arr[i]=arr[i-1];
+            }
+            else if(arr[i+1]!=-1){
+                arr[i]=arr[i+1];
+            }
+        }
+    }
+    printf("RECOVERED ");
+    for(int i=0; i<N; i++){
+        printf("%d ", arr[i]);
+        jumlah = jumlah + arr[i];
+    }
+    printf("\n");
+    printf("MAX_SUM %d", jumlah);
 }
