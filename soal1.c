a8336c07e512539d3b371dde5e8111ae4af8cab9
Ihsan Arya <ihsanaryafiisp@gmail.com>
2026-04-20 08:43:17 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -16,6 +16,9 @@ int main(){
     int jumlah = 0;
     scanf("%d", &N);
     int arr[N];
+    for(int i=0; i<=N; i++){
+        arr[i]=-1;
+    }
     for(int i=0; i<N; i++){
         scanf("%d", &arr[i]);
     }
